63b27a615085b76604cd835e3af297a66c912fcf
Nathan Fox <nathanfox0102@gmail.com>
2024-07-10 17:50:04 -0500

    edit

--- src/scripts/youtube.js
+++ src/scripts/youtube.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const KEY = "AIzaSyAPvZNTWn9sh9XkZiwpHJc1EhrnMV0pltI";
+const KEY = "";
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
